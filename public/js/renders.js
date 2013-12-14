3816da2dbaa2424b2bf27540e7a2fd57bacf864b
jaisor <jaisor@gmail.com>
2013-12-14 01:08:25 -0800

    switched to SB Admin dashboard theme

--- public/js/renders.js
+++
@@ -1,7 +0,0 @@
-function renderSummary(data) {
- // var template = $('#summary-tpl').html();
- //    return Hogan.compile(template).render({
- //    	test: "Testin!"
- //    });
-	return prettyPrint(data);
-}
